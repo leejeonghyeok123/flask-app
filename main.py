7ead03798688a75748707ae0e68989904c44cbb4
unknown <powey1358@naver.com>
2024-01-04 22:04:07 +0900

    flask test

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask , Blueprint , jsonify
+
+app = Flask(__name__)
+
+app_api = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+
+
+@app_api.route('/health')
+def health():
+    response_data = {'message': 'true'}
+    return jsonify(response_data) , 200
+
+if __name__ == '__main__':
+    # 블루프린트 등록
+    app.register_blueprint(app_api)
+    app.run(debug=True)
